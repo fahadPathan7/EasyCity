ff859160ccb0fc8ec4f32943c82f0e225fb3d582
Fahad Pathan <fahadpathan56@gmail.com>
2024-03-26 14:21:54 +0600

    update: __dirname changed

--- Backend/app.js
+++ Backend/app.js
@@ -36,7 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // set static folder
-app.use(express.static(path.join(__dirname, "public")));
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 // parse cookies
 app.use(cookieParser(process.env.COOKIE_SECRET));
